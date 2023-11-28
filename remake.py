42b59fb9110ddb76c0051f4f9bde90f48d312350
User-Shabab <141272947+User-Shabab@users.noreply.github.com>
2023-11-28 22:18:22 +0000

    Add files via upload

---
+++ remake.py
@@ -0,0 +1,34 @@
+subject = input("Subject? (...)_")
+topic = input("Topic? (...)_")
+subtopic = input("Subtopic? (...)_")
+
+def fdiseases(s):
+    if s == "virus":
+        with open("virus.txt", 'r') as f:
+            ans2 = f.read()
+        return ans2
+    if s == "bacteria":
+        with open("bacteria.txt", 'r') as f:
+            ans2 = f.read()
+        return ans2
+    if s == "protist":
+        with open("protist.txt", 'r') as f:
+            ans2 = f.read()
+        return ans2
+    if s == "fungi":
+        with open("fungi.txt", 'r') as f:
+            ans2 = f.read()
+        return ans2
+
+def fbiology(t):
+    if t == "disease":
+        ans1 = fdiseases(subtopic)
+        return ans1
+    else:
+        return "Subtopic not found."
+
+if subject.lower() in ['b', 'bio', 'biology']:
+    ans = fbiology(topic)
+    print(ans)
+else:
+    print("Invalid subject.")
